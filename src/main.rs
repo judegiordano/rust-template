1e1eb40f08cb2647db06f0efa66e26c32915d72f
judegiordano <judegiordano@gmail.com>
2022-06-28 01:21:19 -0400

    Config Builder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::utils::config::Config;
+
 extern crate tokio;
 
 pub mod tests;
@@ -5,5 +7,7 @@ pub mod utils;
 
 #[tokio::main]
 async fn main() -> Result<(), ()> {
+    let config: Config = utils::config::Config::new();
+    println!("{:#?}", config);
     Ok(())
 }
---
+++ src/utils/config.rs
@@ -0,0 +1,28 @@
+use dotenv::dotenv;
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::str::FromStr;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Config {
+    pub rust_backtrace: u32,
+}
+
+pub fn parse_env_variable<T: FromStr>(name: &str) -> T {
+    // get variable
+    let normalized: String = name.trim().to_uppercase().trim().to_string();
+    let value: String = env::var(&normalized).unwrap_or_else(|_| panic!("{} not set", &normalized));
+    // parse data type
+    value
+        .parse::<T>()
+        .unwrap_or_else(|_| panic!("error parsing environment variable {}", &normalized))
+}
+
+impl Config {
+    pub fn new() -> Config {
+        dotenv().ok();
+        Config {
+            rust_backtrace: parse_env_variable::<u32>("RUST_BACKTRACE"),
+        }
+    }
+}
